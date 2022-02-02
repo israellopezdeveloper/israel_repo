3761c1f7eafa278fd5dce3fd51e06db85ec29d29
Israel Lopez <israel.lopez.developer@gmail.com>
2022-02-02 21:53:15 +0100

    commit

--- installKoala.sh
+++ installKoala.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+sudo -u root passwd
+
 sudo -u root -i <<EOF
-passwd
 echo -e "[isra_repo]\nSigLevel= Optional TrustedOnly\nServer = https://israellopezdeveloper.github.io/israel_repo/" >> /etc/pacman.conf
 pacman -Syyu --noconfirm koala-base
 useradd -m -g users -G audio,lp,optical,storage,video,wheel,games,power,scanner -s /bin/zsh israel
