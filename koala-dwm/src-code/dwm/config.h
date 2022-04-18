19e05ad929a887d2550de46ad47cb794b3221a3f
Israel Lopez <israel.lopez.developer@gmail.com>
2022-04-18 23:53:18 +0200

    commit

--- koala-dwm/src-code/dwm/config.h
+++ koala-dwm/src-code/dwm/config.h
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 
 /* tagging */
 static const char *tags[] = { "", "", "", "", "", "", "₿", "", "" };
-static const char *tagsalt[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tagsalt[] = { "一", "二", "三", "四", "五", "六", "七", "八", "九" };
 static const int momentaryalttags = 0; /* 1 means alttags will show only when key is held down*/
 
 static const Rule rules[] = {
